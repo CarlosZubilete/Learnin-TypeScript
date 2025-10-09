48bc8e32ac6b35f48a7efb18c91847b189f952e2
CZubileteSanchez <carloszubilete@gmail.com>
2025-10-09 04:43:55 -0300

    feat: created enum.ts into variables&notations

--- variables&notations/enum.ts
+++ variables&notations/enum.ts
@@ -1,6 +1,6 @@
-// enum nameEnum { value = key , value = key }
+// enum nameEnum { key = value , key = value }
 
-// By default, TypeScript gives numbers starting at 0 = { 0 ,1 , 2, 3 , ...}
+// 1. By default, TypeScript gives numbers starting at 0 = { 0 ,1 , 2, 3 , ...}
 enum WeekDays {
   Monday,
   Tuesday,
@@ -14,7 +14,7 @@ enum WeekDays {
 let dayNumber: WeekDays = WeekDays.Wednesday;
 console.log("dayNumber: ", dayNumber); // 2
 
-// Value defined: You can set your own numbers.
+// 2. Value defined: You can set your own numbers.
 enum Colors {
   Red = 2,
   Green = 4,
@@ -24,7 +24,7 @@ enum Colors {
 let color: string = Colors[4];
 console.log("color: ", color); // Green: (only works with numbers)
 
-// look at this "string enums"
+// 3. look at this "string enums"
 enum Month {
   January = "first",
   February = "second",
@@ -33,12 +33,12 @@ enum Month {
   May = "fifth",
 }
 
-// ERROR -> With string enums, you can only go one way (key → value).
+// ERROR -> With string enums, you can only go one way (key -> value):
 // let month: string = Month["fifth"];
 //
 let month: string = Month.May;
 console.log("month: ", month); // "fifth"
 
-// Allows to define a structure of fixed numerical values.
-// They are associated with expression.
-// They ara commonly used in interface design.
+// *Allows to define a structure of fixed numerical values.
+// *They are associated with expression.
+// *They ara commonly used in interface design.
